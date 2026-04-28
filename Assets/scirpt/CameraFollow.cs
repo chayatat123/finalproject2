3c7f844cea0c298071cef8e31e6a82a496b3ca10
chayatat123 <chayatat.seer@bumail.net>
2026-04-28 16:09:25 +0700

    camera

--- Assets/scirpt/CameraFollow.cs
+++ Assets/scirpt/CameraFollow.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform ball;     // ลากลูกบอลมาใส่ตรงนี้
+    public Transform player;   // ลากลูกบอลมาใส่
     public float smoothSpeed = 5f;
 
     void LateUpdate()
     {
         Vector3 targetPos = new Vector3(
-            ball.position.x,
-            transform.position.y,
+            player.position.x,
+            player.position.y,
             transform.position.z
         );
 
