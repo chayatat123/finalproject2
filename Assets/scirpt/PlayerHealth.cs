bdfe5994ce3db35503a752ac9cbd2a48f38b66c1
pongsawatputt-creator <pongsawat.putt@bumail.net>
2026-04-30 15:42:03 +0700

    HPBar

--- Assets/scirpt/PlayerHealth.cs
+++ Assets/scirpt/PlayerHealth.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     private int currentHealth;
 
+    public Slider hpSlider;
+
     void Start()
     {
         currentHealth = maxHealth;
+
+        if (hpSlider != null)
+        {
+            hpSlider.maxValue = maxHealth;
+            hpSlider.value = currentHealth;
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        if (hpSlider != null)
+        {
+            hpSlider.value = currentHealth;
+        }
+
         Debug.Log("HP: " + currentHealth);
 
         if (currentHealth <= 0)
