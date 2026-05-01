325a798361dd8d18c8ed8f48595cd500cb088527
krittidech421 <krittidech.sris@bumail.net>
2026-05-01 20:04:46 +0700

    fix

--- Assets/scirpt/DeadPoint.cs
+++ Assets/scirpt/DeadPoint.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (col.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            col.GetComponent<PlayerRespawn>().Die();
+            PlayerRespawn player = collision.GetComponent<PlayerRespawn>();
+            if (player != null)
+            {
+                player.Respawn();
+            }
         }
     }
 }
--- Assets/scirpt/SpawnPoint.cs
+++ Assets/scirpt/SpawnPoint.cs
@@ -1,38 +1,11 @@
 using UnityEngine;
-using System.Collections;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    public Transform spawnPoint;
-    public float respawnDelay = 1f;
+    public Transform spawnPoint; // จุดเกิด
 
-    private bool isDead = false;
-
-    void Start()
-    {
-        Respawn();
-    }
-
-    public void Die()
-    {
-        if (isDead) return;
-        isDead = true;
-
-        // ปิดการควบคุม (ถ้ามี script movement ก็ปิดตรงนี้)
-        GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
-
-        StartCoroutine(RespawnDelay());
-    }
-
-    IEnumerator RespawnDelay()
-    {
-        yield return new WaitForSeconds(respawnDelay);
-        Respawn();
-    }
-
-    void Respawn()
+    public void Respawn()
     {
         transform.position = spawnPoint.position;
-        isDead = false;
     }
 }
